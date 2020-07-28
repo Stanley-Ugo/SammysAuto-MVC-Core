0d81821c1c1a458c70fd25009ba7ecf8759057ca
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-28 02:13:36 +0100

    adding Details view to Details action

--- Controllers/ServiceTypesController.cs
+++ Controllers/ServiceTypesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SammysAuto.Data;
 using SammysAuto.Models;
 
@@ -42,6 +43,21 @@ namespace SammysAuto.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(serviceTypes);
+        }
+
+        //DETAILS: ServiveTypes/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
         }
         protected override void Dispose(bool disposing)
         {
