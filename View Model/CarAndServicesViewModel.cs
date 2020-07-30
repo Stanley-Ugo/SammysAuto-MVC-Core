93353e9d01bbdccf2cdb369b5842e3beec972a7e
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-30 23:12:32 +0100

    adding a CarAndServiceViewModel

---
+++ View Model/CarAndServicesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SammysAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.View_Model
+{
+    public class CarAndServicesViewModel
+    {
+        public Car CarObj { get; set; }
+        public Service NewServiceObj { get; set; }
+        public IEnumerable<Service> PastServiceObj { get; set; }
+        public List<ServiceType> ServiceTypesObj { get; set; }
+    }
+}
