5c3b029b30913ed661f0cfe19fa380fd95e02c20
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-30 01:43:20 +0100

    adding a Car Model with a FK of SammysAutoUser & a migration to add table in Db

---
+++ Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using SammysAuto.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Style { get; set; }
+        public string VIN { get; set; }
+        public int Year { get; set; }
+        public double Miles { get; set; }
+        public string color { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual SammysAutoUser SammysAutoUser { get; set; }
+    }
+}
