1671cfd3d6d37e1007db9d35398ab3e9acab77a9
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-30 11:27:58 +0100

    Adding CarAndCustomerViewModel for Getting respective Objects

---
+++ View Model/CarAndCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SammysAuto.Data;
+using SammysAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.View_Model
+{
+    public class CarAndCustomerViewModel
+    {
+        public SammysAutoUser UserObj { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
