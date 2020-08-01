e4ba36d11848498b4461cf65f4a9a45757668c64
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-01 19:23:28 +0100

    adding properties to the manage Accout Page

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -31,6 +31,25 @@ namespace SammysAuto.Areas.Identity.Pages.Account.Manage
         [BindProperty]
         public InputModel Input { get; set; }
 
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+
         public class InputModel
         {
             [Phone]
